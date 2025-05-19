f98a34e9f7018d3b357f600c64d8db34e33f155d
BiG A <antoine.salameh.45@gmail.com>
2025-05-19 20:51:48 +0300

    feat: add new SVG icons and update dependencies
    - Added new SVG icons: AddSvg, CartSvg, FlashSvg, FlashOffSvg, HomeSvg, and ProfileSvg.
    - Created an index file for easy import of SVG icons.
    - Updated package.json to include new dependencies for FontAwesome, React Navigation, Axios, and React Native SVG.

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,6 @@
+export { default as HomeIcon } from './HomeSvg.svg';
+export { default as CartIcon } from './CartSvg.svg';
+export { default as ProfileIcon } from './ProfileSvg.svg';
+export { default as FlashIcon } from './FlashSvg.svg';
+export { default as FlashOffIcon } from './FlashOffSvg.svg';
+export { default as AddIcon } from './AddSvg.svg';

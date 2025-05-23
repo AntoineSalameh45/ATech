40cd82dca20e282efdcac34d699165de0ccc07d8
BiG A <antoine.salameh.45@gmail.com>
2025-05-23 10:03:06 +0300

    feat: integrate AsyncStorage for cart persistence; refactor CartStore to use zustand middleware for state management

--- src/stores/CartStore/index.tsx
+++ src/stores/CartStore/index.tsx
@@ -1,4 +1,7 @@
-import {create} from 'zustand';
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import type { PersistStorage } from 'zustand/middleware';
 
 interface CartItem {
   _id: string;
@@ -14,33 +17,53 @@ interface CartState {
   removeItem: (id: string) => void;
   clearCart: () => void;
 }
+const asyncStoragePersist: PersistStorage<CartState> = {
+  getItem: async (name) => {
+    const value = await AsyncStorage.getItem(name);
+    return value ? JSON.parse(value) : null;
+  },
+  setItem: async (name, value) => {
+    await AsyncStorage.setItem(name, JSON.stringify(value));
+  },
+  removeItem: async (name) => {
+    await AsyncStorage.removeItem(name);
+  },
+};
 
-const useCartStore = create<CartState>(set => ({
-  cartItems: [],
-  addItem: item =>
-    set(state => {
-      const existingItem = state.cartItems.find(
-        cartItem => cartItem._id === item._id && cartItem.name === item.name,
-      );
+const useCartStore = create<CartState>()(
+  persist(
+    (set) => ({
+      cartItems: [],
+      addItem: (item) =>
+        set((state) => {
+          const existingItem = state.cartItems.find(
+            (cartItem) =>
+              cartItem._id === item._id && cartItem.name === item.name,
+          );
 
-      if (existingItem) {
-        return {
-          cartItems: state.cartItems.map(cartItem =>
-            cartItem._id === item._id && cartItem.name === item.name
-              ? {...cartItem, quantity: cartItem.quantity + item.quantity}
-              : cartItem,
-          ),
-        };
-      }
+          if (existingItem) {
+            return {
+              cartItems: state.cartItems.map((cartItem) =>
+                cartItem._id === item._id && cartItem.name === item.name
+                  ? { ...cartItem, quantity: cartItem.quantity + item.quantity }
+                  : cartItem,
+              ),
+            };
+          }
 
-      return {cartItems: [...state.cartItems, item]};
+          return { cartItems: [...state.cartItems, item] };
+        }),
+      removeItem: (id) =>
+        set((state) => ({
+          cartItems: state.cartItems.filter((item) => item._id !== id),
+        })),
+      clearCart: () => set({ cartItems: [] }),
     }),
-
-  removeItem: id =>
-    set(state => ({
-      cartItems: state.cartItems.filter(item => item._id !== id),
-    })),
-  clearCart: () => set({cartItems: []}),
-}));
+    {
+      name: 'cart-storage',
+      storage: asyncStoragePersist,
+    },
+  ),
+);
 
 export default useCartStore;

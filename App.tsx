270fb065d4ed2d2485a1ce07ac47cf2336217819
BiG A <antoine.salameh.45@gmail.com>
2025-05-27 11:29:07 +0300

    Add react-native-bootsplash dependency and include app launcher icon
    - Added "react-native-bootsplash" version 6.3.8 to package.json for improved splash screen functionality.
    - Added new app launcher icon at src/assets/ic_launcher.png.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {ThemeProvider} from './src/stores/ThemeContext';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {NavigationContainer} from '@react-navigation/native';
 import RootStack from './src/navigation/RootStack';
+import BootSplash from 'react-native-bootsplash';
 
 const App = () => {
+  useEffect(() => {
+    BootSplash.hide({fade: true}).then(() => {
+      console.log('BootSplash has been hidden successfully');
+    });
+  }, []);
+
   return (
     <GestureHandlerRootView>
       <ThemeProvider>
